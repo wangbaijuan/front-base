feea2c3925c721d1727e06b669aa4895af94eec7
guanxine <guanxin@k2data.com.cn>
2018-10-28 16:03:00 +0800

    vue curd

---
+++ issue-curd/vue/src/components/bug/BugForm.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div class="jumbotron">
+            <h3>新建 Bug</h3>
+            <!-- <form id="issueInputForm"> -->
+            <form v-on:submit.prevent="formSubmit">
+                <div class="form-group">
+                    <label for="bug-desc">描述 </label>
+                    <input id="bug-desc" v-model="bug.description" type="text" class="form-control" placeholder="描述这个问题..." >               
+                </div>
+                <div class="form-group">
+                    <label for="bug-severity">级别 </label>
+                    <select id="bug-severity" class="form-control" v-model="bug.severity">
+                        <option value="Low">Low</option>
+                        <option value="Medium">Medium</option>
+                        <option value="High">High</option>
+                    </select> 
+                   
+                </div>
+                <div class="form-group">
+                    <label for="bug-assignedTo">分配  </label>
+                    <input id="bug-assignedTo" v-model="bug.assignedTo" type="text" class="form-control" placeholder="分配给谁 ...">
+                </div>
+                <!-- <button type="submit" class="btn btn-primary" @click="btnSubmit">Add</button> -->
+                <button type="submit" class="btn btn-primary" >Add</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BugFrom',
+    data () {
+        return {
+            bug: {},
+        };
+    },
+    methods: {
+        /* form > input click*/
+        btnSubmit: function(e) {
+            console.log('btn', e)
+            console.log(this.bug.description)
+            this.$emit('submit', this.bug);
+        },
+
+        /* form + submit(禁止默认行为) > input+submit or button
+                    <form id="issueInputForm" v-on:submit.prevent="formSubmit">
+        */
+        formSubmit: function(e) {
+            console.log('form', e)
+            this.$emit('submit', this.bug);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ issue-curd/vue/src/view/bug/index.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="container">
+        <h1> Bug 管理</h1>
+        <BugForm @submit="add">
+
+        </BugForm>
+
+        <div class="row">
+             <BugItem
+                    v-for="item in bugs" 
+                    :key="item.id"
+                    :bug="item"
+                    @close="close"
+                    @remove="remove"
+                    >
+            </BugItem>
+
+            <!-- <ModalDelete 
+                :modalId="1"
+                @delete="deleteBug" 
+                :recordId="deleteId"
+                :title="Bug"> -->
+        </div>
+    </div>
+</template>
+
+<script>
+
+import BugForm from "~/components/bug/BugForm";
+import BugItem from "~/components/bug/BugItem";
+// import ModalDelete from "~/components/ModalDelete";
+export default {
+    name : "bug",
+    components: {
+        BugForm,BugItem,
+        // ModalDelete
+    },
+    data(){
+        return {
+            bug:{},
+            bugs: []
+        }
+    },
+    methods: {
+        add(bug) {
+            console.log('index.vue add bug', bug.description);
+        },
+        remove(id){
+            console.log("remove:" + id);
+        },
+        close(id) {
+            console.log("close:" + id)
+        }
+
+    },
+    created() {
+        this.bugs = [
+            {
+                id: "1",
+                description: "desc1",
+                severity: "low",
+                status: "Open",
+                assignedTo:"shaonian"
+            },
+             {
+                id: "2",
+                description: "desc1",
+                severity: "low",
+                status: "Close",
+                assignedTo:"shaonian"
+            }
+        ]
+    },
+
+}
+</script>
+
+<style>
+
+</style>
