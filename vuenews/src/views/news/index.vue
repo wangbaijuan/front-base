4bc3e763b85e4551deb441af700c83ed81bb4848
guanxine <guanxin@k2data.com.cn>
2018-10-28 16:31:37 +0800

    issue tracker

---
+++ vuenews/src/views/news/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container" id="app">
+    <SourceSelection v-on:sourceChanged="sourceChanged"></SourceSelection>
+    <Newslist v-bind:source="source"></Newslist>
+  </div>
+</template>
+<script>
+/* eslint-disable */ 
+import Newslist from '@/components/news/Newslist'
+import SourceSelection from '@/components/news/SourceSelection'
+export default {
+  name: 'news',
+  components: {
+    Newslist,
+    SourceSelection
+  },
+  data () {
+    return {
+      source: ''
+    }
+  },
+  methods: {
+    sourceChanged: function (source) {
+      this.source = source
+    }
+  }
+}
+</script>
+<style>
+  #app {
+    padding-top: 20px
+  }
+</style>
